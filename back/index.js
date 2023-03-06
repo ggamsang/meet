77f03b5dd5ed8de597d1d1e3a858bf96a7df16a1
ggamsang <w.ggmsng@gmail.com>
2023-03-06 16:09:25 +0900

    local connection setting

--- back/index.js
+++ back/index.js
@@ -1,30 +1,28 @@
-
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: { origin: "http://127.0.0.1:8080" },
+});
+const cors = require("cors");
 
 app.use(express.static("/home/ubuntu/place/front/dist/"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.get("/message", (req, res) => {
-	res.send("ffsd");
+  res.send("ffsd");
 });
 app.post("/messages", (req, res) => {
-	console.log(req.body);
-	io.emit("message", req.body);
-	res.sendStatus(200);
-
+  console.log(req.body);
+  io.emit("message", req.body);
+  res.sendStatus(200);
 });
 io.on("connection", (socket) => {
-	console.log("a user is connected: ",socket.id);
-	socket.emit("welcom",socket.id);
-	
+  console.log("a user is connected: ", socket.id);
+  socket.emit("welcom", socket.id);
 });
 const server = http.listen(3000, () => {
-	console.log("server is running on port: ", server.address().port);
+  console.log("server is running on port: ", server.address().port);
 });
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-import { io } from 'socket.io-client';
+import { useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
 
 function App() {
   const [connected, setConnected] = useState(false);
   let _socket = null;
   const init_socket = () => {
-	  _socket = io().on("welcom",(data)=>{console.log(data);setConnected();});
-  }
-  const send_message = (msg) => {
-
-  }
+    _socket = io("http://127.0.0.1:3000").on("welcom", (data) => {
+      console.log(data);
+      setConnected();
+    });
+  };
+  const send_message = (msg) => {};
 
   return (
-<div className="App">
-{!connected 
-	?
-	<button onClick={()=>init_socket()}>connect to socket server...</button>
-	:
-	<>
-	connected<br/>
-	<input type="text"/><button>send</button>
-	<>
-}
-<div>
-</div>
-</div>
-  )
+    <div className="App">
+      {!connected ? (
+        <button onClick={() => init_socket()}>
+          connect to socket server...
+        </button>
+      ) : (
+        <>
+          connected
+          <br />
+          <input type="text" />
+          <button>send</button>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
